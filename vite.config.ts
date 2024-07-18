2db3978e7b795e8f7cf95e1650559db8cf3ca7e4
Barret Lemire <blemire4503@xmail.dixietech.edu>
2024-07-17 20:48:59 -0600

    modify pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<repository-name>/',
+  base: '/react-eta/',
   build: {
     outDir: 'dist',
   },
